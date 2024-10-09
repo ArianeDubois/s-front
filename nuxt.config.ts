f0304c72014be35b9a7e57ae147685146b9d0b84
ArianeDubois <36155355+ArianeDubois@users.noreply.github.com>
2024-10-09 17:31:07 +0200

    TEST SSR nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,8 @@ import process from 'node:process'
 
 export default defineNuxtConfig({
   modules: ['@vueuse/nuxt', 'nuxt-kql', '@nuxt/image'],
-  // ssr: true, // Activer SSR
-  // target: 'server', // Utiliser un serveur pour le rendu
-  // buildModules: [
-  //   '@nuxt/image',
-  //   {
-  //     provider: 'static',
-  //   },
-  // ],
-
+  ssr: true, // Activer SSR
+  target: 'server', // Utiliser un serveur pour le rendu
   // app: {
   //   pageTransition: { name: 'zoom' },
   // },
@@ -57,9 +50,10 @@ export default defineNuxtConfig({
   },
 
   nitro: {
-    prerender: {
-      // Prerender the index page
-      routes: ['/'],
-    },
+    preset: 'node-server',
+    // prerender: {
+    //   // Prerender the index page
+    //   routes: ['/'],
+    // },
   },
 })
